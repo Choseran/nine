cbbd6f008d858241d71f37937df699d8ae9cb895
seran <rldlfdj@naver.com>
2019-04-03 10:35:56 +0900

    세상을 줄게

--- t/src/t/push.java
+++ t/src/t/push.java
@@ -3,6 +3,6 @@ package t;
 public class push {
 	public static void main(String[]args) {
 		System.out.println("add java class file push");
-		System.out.println("add java class file pussssshhh");
+		System.out.println("world is yours");
 	}
 }

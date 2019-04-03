eb414f4fb5305b62ab2c14a97e0b0f1ac162e5e0
seran <rldlfdj@naver.com>
2019-04-03 10:42:48 +0900

    edit

--- t/src/t/push.java
+++ t/src/t/push.java
@@ -3,7 +3,7 @@ package t;
 public class push {
 	public static void main(String[]args) {
 		System.out.println("add java class file push");
-		System.out.println("world is yours");
+		System.out.println("world is your");
 		System.out.println("world with 42");
 	}
 }

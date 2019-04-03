726a9f44ed73d72cff702ee11731b550ba8cf98e
seran <rldlfdj@naver.com>
2019-04-03 09:38:47 +0900

    asd

--- t/src/t/push.java
+++ t/src/t/push.java
@@ -3,5 +3,6 @@ package t;
 public class push {
 	public static void main(String[]args) {
 		System.out.println("add java class file push");
+		System.out.println("add java class file pushhh");
 	}
 }
